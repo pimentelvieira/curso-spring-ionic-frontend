8707aa29788ca35d83a02549a937efa6563919cf
William Pimentel <pimentelvieira@yahoo.com.br>
2018-09-15 17:35:32 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public cartService: CartService,
     public clienteService: ClienteService, public pedidoService: PedidoService) {
@@ -51,15 +52,23 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout() {
     this.pedidoService.insert(this.pedido).subscribe(response => {
       this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codpedido = this.extractId(response.headers.get('location'));
     },
     error => {
       if (error.status == 403) {
         this.navCtrl.setRoot('HomePage');
       }
     });
+  }
+
+  private extractId(location: string) : string {
+    return location.substring(location.lastIndexOf('/') + 1, location.length);
   }
 }
